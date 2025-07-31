a960a4ee7cf580da253c98cc03e4979f3890eb03
siddhant-2029 <work.siddhantk@gmail.com>
2025-07-31 11:54:56 +0530

    bitwise operators

--- BitwiseOperator/bitwiseoperators.cpp
+++ BitwiseOperator/bitwiseoperators.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 { int a=7,b=8;
     //bitwise and operator &
-    cout<<(a & b)<<endl;
+    cout<<(a&b)<<endl;
     //here now in decimal a=7=111 and b=8=1000 so & of a and b will be 0000 or 0
 
     //bitwise or operaator |
@@ -14,6 +14,14 @@ int main()
     cout<<(a^b)<<endl;
     // xror works like 1 for unlikes and 0 for like so for a and b it will be 1111 or 15
    
+    // right shift operator >>
+    cout<<(a>>2)<<endl;
+    // here right shift a which was 111 shifted by two places so it will be 001 so output will 1
+
+    // left shift perator <<
+    cout<<(a<<2)<<endl;
+    //and here it shift to left by 2 so a which was 111 will become 11100 or 4+8+16=28
+
    
    
     return 0;
